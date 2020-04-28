0b16f0856eceff633a8b7a6543482c9d79885e95
Jie Lou <jie.lou.data@gmail.com>
2020-04-28 08:04:07 -0400

    stack implementation

---
+++ data structure and algorithm/python_implementation/stack.py
@@ -0,0 +1,22 @@
+# stack implementation is easier that that in Java.
+# reference: https://www.geeksforgeeks.org/stack-in-python/
+# option 1 using list. but slow, not efficient
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self, value):
+        if self.stack:
+            self.stack.pop()
+        else:
+            print("stack is empty")
+
+# option 2 using deque. but slow, not efficient
+def deque_example():
+    from collections import deque
+    stack = deque()
+    stack.append("a") #O(1)
+    stack.pop() #O(1)
