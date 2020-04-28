54754987d5ca791b88b8991cbd060f3af8b1d729
Jie Lou <jie.lou.data@gmail.com>
2020-04-28 08:03:43 -0400

    finish implementatioini in linkedin

--- data structure and algorithm/python_implementation/linkedlist.py
+++ data structure and algorithm/python_implementation/linkedlist.py
@@ -10,10 +10,12 @@ class LinkedList:
         self.head = None
 
     def add_first(self, value):
+        # O(1)
         self.head = Node(value, self.head)
     
     def add_last(self, value):
         # if the list empty
+        # O(n)
         if self.head is None:
             self.add_first(value)
         else:
@@ -23,13 +25,49 @@ class LinkedList:
             tmp.next = Node(value, None)
 
     def insert_after(self, key, value):
-        pass
+        tmp = self.head
+        while tmp is not None:
+            if tmp.data==key:
+                tmp.next = Node(value,tmp.next)
+                return
+            tmp = tmp.next
+        print(f"no [{key}] found")
+        
 
     def insert_before(self, key, value):
-        pass
+        if self.head.data==key:
+            self.add_first(value)
+            return
+        
+        first_pointer = self.head
+        second_pointer = self.head.next
 
-    def remove(self, key, value):
-        pass
+        while second_pointer is not None:
+            if second_pointer.data==key:
+                first_pointer.next=Node(value,second_pointer)
+                return
+            second_pointer = second_pointer.next
+            first_pointer = first_pointer.next
+        print(f"no [{key}] found")
+
+    def remove(self, key):
+        if self.head is None: # empty
+            return
+        if self.head.data == key:
+            self.head = self.head.next
+            return
+
+        first_pointer = self.head
+        second_pointer = self.head.next
+        
+        while second_pointer is not None:
+            if second_pointer.data == key: # if found
+                first_pointer.next = second_pointer.next
+                return
+            second_pointer = second_pointer.next
+            first_pointer = first_pointer.next
+        print(f"no [{key}] found")
+
 
     def __str__(self):
         if self.head is None:
@@ -51,4 +89,24 @@ if __name__ == "__main__":
     linked_list.add_first("haha")
     print(linked_list)
     linked_list.add_last("end")
+    print(linked_list)
+    linked_list.insert_after("haha", "after haha")
+    print(linked_list)
+    linked_list.insert_after("end", "after end")
+    print(linked_list)
+    linked_list.insert_after("not found", "after not found")
+    print(linked_list)
+    linked_list.insert_before("end", "before end")
+    print(linked_list)
+    linked_list.insert_before("haha", "before haha")
+    print(linked_list)
+    linked_list.insert_before("not found", "before not found")
+    print(linked_list)
+    linked_list.remove("after end")
+    print(linked_list)
+    linked_list.remove("before end")
+    print(linked_list)
+    linked_list.remove("after haha")
+    print(linked_list)
+    linked_list.remove("before haha")
     print(linked_list)
